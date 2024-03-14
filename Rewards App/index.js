c01d4c2e30aed99a1d3f7888f86d5a0f16bee0bc
BootsAndCodes <bermie92@gmail.com>
2024-03-14 23:24:32 +0100

    Uncaught TypeError?

---
+++ Rewards App/index.js
@@ -0,0 +1,4 @@
+document.getElementById('achievements-button').addEventListener('click', function() {
+    const achievementsContainer = document.querySelector('.achievements-container');
+    achievementsContainer.classList.toggle('content-hidden');
+});
